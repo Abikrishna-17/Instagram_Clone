8167bc4bc04fc629844719b5b7c14a32a1a133ef
Abikrishna <abikrishnaviswanathan@gmail.com>
2026-08-07 14:57:04 +0530

    homepage

--- home_page.js
+++ home_page.js
@@ -1,3 +1,15 @@
+const arr = ["img/img1.jpeg","img/img2.jpeg","img/img3.jpeg","img/img4.jpeg","videos/1080x1920.mp4"];
+const dummy_arr = [...arr];
+const display_queue = [];
 
-const arr = [1,2,3,4,5];
-console.log(arr[Math.floor(Math.random()*arr.length)])
+let n = arr.length;
+while(n>0){
+
+    const post = arr[Math.floor(Math.random()*arr.length)]
+    if(!display_queue.includes(post)){
+        display_queue.push(post);
+        n--;
+    }
+}
+
+console.log(display_queue)

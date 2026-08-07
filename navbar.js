614cf0ba3196e1d98e7c89dc65fdcaa9b38e569b
Abikrishna <abikrishnaviswanathan@gmail.com>
2026-08-07 14:11:32 +0530

    initial commit

---
+++ navbar.js
@@ -0,0 +1,30 @@
+const navbar = `
+<nav>
+    <nav id="home_navbar">
+            <a href="home_page.html" >
+            <h3>
+                Home
+            </h3>
+            </a>
+            <a href="post_page.html" >
+            <h3>
+                Post
+            </h3>
+            </a>
+            <a href="message_page.html" >
+            <h3>
+                Message
+            </h3>
+            <a href="reels_page.html" >
+            <h3>
+                Reels
+            </h3>
+            </a>
+            <a href="profile_page.html" >
+            <h3>
+                Profile
+            </h3>
+            </a>
+        </nav>
+`;
+document.getElementById("navbar").innerHTML = navbar;
---
+++ profile_page.js
@@ -0,0 +1,51 @@
+async function loadprofile() {
+
+        const API = "https://6a66fb27189fe5869eb6cfc7.mockapi.io/api/user/passwords";
+
+        const userid = localStorage.getItem("userid");
+
+        const user = await fetch(`${API}/${userid}`);
+        const res = await user.json();
+
+        document.getElementById("name").textContent = `Name : ${res.name}`;
+        document.getElementById("username").textContent = `Username : ${res.username}`;
+        document.getElementById("gender").textContent = `Gender : ${res.gender}`;
+    
+        let dobb = document.getElementById("dob")
+        let rev_dob = res.dob.split("-").reverse().join(":");
+        dobb.textContent = `Date of Birth : ${rev_dob}`;
+
+        let joined = document.getElementById("joined")
+        let rev_createdAt = res.createdAt.substring(0,10).split("-").reverse().join(":");
+        joined.textContent = `Joined On : ${rev_createdAt}`;
+
+    }
+loadprofile();
+
+
+const logout_button = document.getElementById("logout_button");
+
+logout_button.addEventListener("click",()=>{
+    localStorage.removeItem('userid');
+    window.location.href = "login_page.html";
+})
+
+const delete_button = document.getElementById("delete_button");
+delete_button.addEventListener("click",deleteAccount);
+
+async function deleteAccount(){
+
+    if(confirm("Delete account?")){
+
+    const API = "https://6a66fb27189fe5869eb6cfc7.mockapi.io/api/user/passwords";
+    const userid = localStorage.getItem("userid");
+    const user = await fetch(`${API}/${userid}`,{
+        method:"DELETE"
+    });
+
+    localStorage.removeItem("userId");
+    alert("Account deleted successfully.");
+    window.location.href = "login_page.html";
+
+    }
+}
